10294629ccd5ef1186689418119205e66996db50
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-04-11 07:03:18 -0700

    Setup Travis

--- setup.py
+++ setup.py
@@ -4,45 +4,48 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Adrian Garcia Badaracco",
-    author_email='adrian@adriangb.com',
-    python_requires='>=3.5',
+    author_email="adrian@adriangb.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Scikit-Learn API implementation for Keras.",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='sklearn_keras_wrap',
-    name='sklearn_keras_wrap',
-    packages=find_packages(include=['sklearn_keras_wrap', 'sklearn_keras_wrap.*']),
+    keywords="sklearn_keras_wrap",
+    name="sklearn_keras_wrap",
+    packages=find_packages(include=["sklearn_keras_wrap", "sklearn_keras_wrap.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/adriangb/sklearn_keras_wrap',
-    version='0.1.0',
+    url="https://github.com/adriangb/sklearn_keras_wrap",
+    version="0.1.0",
     zip_safe=False,
 )
--- sklearn_keras_wrap/__init__.py
+++ sklearn_keras_wrap/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Scikit-Learn Wrapper for Keras."""
 
 __author__ = """Adrian Garcia Badaracco"""
-__email__ = 'adrian@adriangb.com'
-__version__ = '0.1.0'
+__email__ = "adrian@adriangb.com"
+__version__ = "0.1.0"
