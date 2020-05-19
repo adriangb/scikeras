09d2d84c73bd3c232bdc78d1c46be43c676ef7ac
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-05-19 02:09:34 -0700

    remove email

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.md") as history_file:
+    history = history_file.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup_requirements = [
+    "pytest-runner",
+]
+
+test_requirements = [
+    "pytest>=3",
+]
+
+setup(
+    author="Adrian Garcia Badaracco",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Scikit-Learn API implementation for Keras.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="scikeras",
+    name="scikeras",
+    packages=find_packages(include=["scikeras", "scikeras.*"]),
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/adriangb/scikeras",
+    version="0.1.7",
+    zip_safe=False,
+)
