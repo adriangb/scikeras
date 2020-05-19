a446d76c80527d3ca958ec688e8419e9177c1b4a
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-05-19 00:42:06 -0700

    fix quotes around verison number

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/adriangb/scikeras",
-    version="version='0.1.6'",
+    version='0.1.6',
     zip_safe=False,
 )
