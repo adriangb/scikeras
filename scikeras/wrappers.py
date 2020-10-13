dc8d218a6706971480e2558ca4a50c8cfa1b4c9f
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-10-13 09:18:58 -0500

    Style nit

--- scikeras/wrappers.py
+++ scikeras/wrappers.py
@@ -420,23 +420,21 @@ class BaseWrapper(BaseEstimator):
         # check that if the user gave us a loss function it ended up in
         # the actual model
         init_params = inspect.signature(self.__init__).parameters
-        if "loss" not in init_params:
-            # subclassed model does not have the `loss` parameter in it's signature
-            return
-        default_val = init_params["loss"].default
-        if all(
-            isinstance(x, (str, losses_module.Loss, type))
-            for x in [self.loss, self.model_.loss]
-        ):  # filter out loss list/dicts/etc.
-            if default_val is not None:
-                default_val = loss_name(default_val)
-            given = loss_name(self.loss)
-            got = loss_name(self.model_.loss)
-            if given != default_val and got != given:
-                raise ValueError(
-                    f"loss={self.loss} but model compiled with {self.model_.loss}."
-                    " Data may not match loss function!"
-                )
+        if "loss" in init_params:
+            default_val = init_params["loss"].default
+            if all(
+                isinstance(x, (str, losses_module.Loss, type))
+                for x in [self.loss, self.model_.loss]
+            ):  # filter out loss list/dicts/etc.
+                if default_val is not None:
+                    default_val = loss_name(default_val)
+                given = loss_name(self.loss)
+                got = loss_name(self.model_.loss)
+                if given != default_val and got != given:
+                    raise ValueError(
+                        f"loss={self.loss} but model compiled with {self.model_.loss}."
+                        " Data may not match loss function!"
+                    )
 
     def _validate_data(
         self, X, y=None, reset: bool = False
