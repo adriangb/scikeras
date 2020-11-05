d9e11ed5b30cafbf0c53e918c312fbe3e79148ed
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-11-04 22:39:25 -0600

    Add test

---
+++ tests/test_basewrapper.py
@@ -0,0 +1,59 @@
+"""Test that BaseWrapper for uses other than KerasClassifier and KerasRegressor.
+"""
+import numpy as np
+
+from sklearn.metrics import mean_squared_error
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from scikeras.wrappers import BaseWrapper
+
+
+class TestAutoencoder:
+    def test_simple_autoencoder_mnist(self):
+        """Tests an autoencoder following .
+        """
+        # Data
+        (x_train, _), (x_test, _) = keras.datasets.mnist.load_data()
+        x_train = x_train.astype("float32") / 255.0
+        x_test = x_test.astype("float32") / 255.0
+        x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+        x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+        # Model
+        encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+        # This is our input image
+        input_img = keras.Input(shape=(784,))
+        # "encoded" is the encoded representation of the input
+        encoded = layers.Dense(encoding_dim, activation="relu")(input_img)
+        # "decoded" is the lossy reconstruction of the input
+        decoded = layers.Dense(784, activation="sigmoid")(encoded)
+        # This model maps an input to its reconstruction
+        autoencoder_model = keras.Model(input_img, decoded)
+        # This model maps an input to its encoded representation
+        encoder_model = keras.Model(input_img, encoded)
+        # This is our encoded (32-dimensional) input
+        encoded_input = keras.Input(shape=(encoding_dim,))
+        # Retrieve the last layer of the autoencoder model
+        decoder_layer = autoencoder_model.layers[-1]
+        # Create the decoder model
+        decoder_model = keras.Model(encoded_input, decoder_layer(encoded_input))
+
+        # Wrap model
+        compile_params = {"optimizer": "adam", "loss": "binary_crossentropy"}
+        fit_params = {"epochs": 20, "batch_size": 256, "shuffle": True}
+
+        autoencoder = BaseWrapper(
+            model=autoencoder_model, **compile_params, **fit_params
+        )
+        encoder = BaseWrapper(model=encoder_model, **compile_params, **fit_params)
+        decoder = BaseWrapper(model=decoder_model, **compile_params, **fit_params)
+
+        # Training
+        autoencoder.fit(x_train, x_train)
+        encoder.fit(X=x_train)
+        encoded_img = encoder.predict(x_test)
+        decoder.fit(X=encoded_img)
+        roundtrip_imgs = decoder.predict(encoder.predict(x_test))
+        mse = mean_squared_error(roundtrip_imgs, x_test)
+        assert mse <= 0.01
