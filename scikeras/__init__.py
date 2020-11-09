b908e110f6a5ae5d887e1988c0afdb2bfe1ad29b
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-11-08 19:33:41 -0600

    Add serialization tests, add serialization hacks for losses, optimizers and metrics, reorganize serialization stuff into a module

--- scikeras/__init__.py
+++ scikeras/__init__.py
@@ -3,12 +3,18 @@
 __author__ = """Adrian Garcia Badaracco"""
 __version__ = "0.2.0"
 
-
-# Monkey patch log_cosh reference
-# See https://github.com/tensorflow/tensorflow/pull/42097
-# Will be removed whenever the
-# min supported version of tf incorporates the fix
-from tensorflow.python import keras  # noqa
+from tensorflow import keras
+from tensorflow.python.keras import metrics as _metrics  # noqa
 
 
-keras.metrics.log_cosh = keras.metrics.logcosh
+_metrics.log_cosh = (
+    keras.metrics.logcosh
+)  # See https://github.com/tensorflow/tensorflow/pull/42097
+
+from scikeras import _saving_utils as saving_utils  # noqa
+
+
+keras.Model.__reduce__ = saving_utils.pack_keras_model
+keras.losses.Loss.__reduce__ = saving_utils.pack_keras_loss
+keras.metrics.Metric.__reduce__ = saving_utils.pack_keras_metric
+keras.optimizers.Optimizer.__reduce__ = saving_utils.pack_keras_optimizer
