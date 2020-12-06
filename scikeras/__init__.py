420fb5db8806b8ff46f1a4adfe0e0f1382ab54a2
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-12-06 12:10:02 -0600

    Prepare for v0.2.1 release

--- scikeras/__init__.py
+++ scikeras/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for Scikit-Learn Wrapper for Keras."""
 
 __author__ = """Adrian Garcia Badaracco"""
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 
 # Monkey patch log_cosh reference
