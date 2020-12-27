ece7ad32a311d23d816983ab0c6c0b3112271e75
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-12-27 12:31:42 -0600

    MAINT: Bump TF version to 2.4.0 (#149)

--- scikeras/__init__.py
+++ scikeras/__init__.py
@@ -4,14 +4,8 @@ __author__ = """Adrian Garcia Badaracco"""
 __version__ = "0.2.1"
 
 from tensorflow import keras
-from tensorflow.python.keras import metrics as _metrics  # noqa
 
-
-_metrics.log_cosh = (
-    _metrics.logcosh
-)  # See https://github.com/tensorflow/tensorflow/pull/42097
-
-from scikeras import _saving_utils as saving_utils  # noqa
+from scikeras import _saving_utils as saving_utils
 
 
 keras.Model.__reduce__ = saving_utils.pack_keras_model
