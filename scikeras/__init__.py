682a6b6cd4ba0c37e45bcef9c9babd62e092fe3b
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-12-28 15:06:10 -0600

    keep saving utils private

--- scikeras/__init__.py
+++ scikeras/__init__.py
@@ -5,10 +5,10 @@ __version__ = "0.2.1"
 
 from tensorflow import keras
 
-from scikeras import _saving_utils as saving_utils
+from scikeras import _saving_utils
 
 
-keras.Model.__reduce__ = saving_utils.pack_keras_model
-keras.losses.Loss.__reduce__ = saving_utils.pack_keras_loss
-keras.metrics.Metric.__reduce__ = saving_utils.pack_keras_metric
-keras.optimizers.Optimizer.__reduce__ = saving_utils.pack_keras_optimizer
+keras.Model.__reduce__ = _saving_utils.pack_keras_model
+keras.losses.Loss.__reduce__ = _saving_utils.pack_keras_loss
+keras.metrics.Metric.__reduce__ = _saving_utils.pack_keras_metric
+keras.optimizers.Optimizer.__reduce__ = _saving_utils.pack_keras_optimizer
