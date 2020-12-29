fcf91ca61679c59c4063f47d42106332ebeebe35
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-12-28 23:13:22 -0600

    avoid importing keras into scikeras.keras

--- scikeras/__init__.py
+++ scikeras/__init__.py
@@ -3,12 +3,12 @@
 __author__ = """Adrian Garcia Badaracco"""
 __version__ = "0.2.1"
 
-from tensorflow import keras
+from tensorflow import keras as _keras
 
 from scikeras import _saving_utils
 
 
-keras.Model.__reduce__ = _saving_utils.pack_keras_model
-keras.losses.Loss.__reduce__ = _saving_utils.pack_keras_loss
-keras.metrics.Metric.__reduce__ = _saving_utils.pack_keras_metric
-keras.optimizers.Optimizer.__reduce__ = _saving_utils.pack_keras_optimizer
+_keras.Model.__reduce__ = _saving_utils.pack_keras_model
+_keras.losses.Loss.__reduce__ = _saving_utils.pack_keras_loss
+_keras.metrics.Metric.__reduce__ = _saving_utils.pack_keras_metric
+_keras.optimizers.Optimizer.__reduce__ = _saving_utils.pack_keras_optimizer
