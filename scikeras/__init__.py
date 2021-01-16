8a5bd8dbc03b4cc4127549bc1b850df7baa94e7b
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-01-16 00:21:55 -0600

    MAINT/ENH: SaveModel based serialization (#128)
    Co-authored-by: Scott <stsievert@users.noreply.github.com>

--- scikeras/__init__.py
+++ scikeras/__init__.py
@@ -3,12 +3,12 @@
 __author__ = """Adrian Garcia Badaracco"""
 __version__ = "0.2.1"
 
+from tensorflow import keras as _keras
 
-# Monkey patch log_cosh reference
-# See https://github.com/tensorflow/tensorflow/pull/42097
-# Will be removed whenever the
-# min supported version of tf incorporates the fix
-from tensorflow.python import keras  # noqa
+from scikeras import _saving_utils
 
 
-keras.metrics.log_cosh = keras.metrics.logcosh
+_keras.Model.__reduce__ = _saving_utils.pack_keras_model
+_keras.losses.Loss.__reduce__ = _saving_utils.pack_keras_loss
+_keras.metrics.Metric.__reduce__ = _saving_utils.pack_keras_metric
+_keras.optimizers.Optimizer.__reduce__ = _saving_utils.pack_keras_optimizer
