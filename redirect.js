addb9da4b025065f0f64efe852b62a8989b0f5d7
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-02-17 01:20:30 -0600

    use real url

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 var base_url = "https://www.adriangb.com/scikeras/";
 
-var current = "https://www.adriangb.com/scikeras/test.html";
+var current = window.location.href;
 var rest = current.replace(base_url, "");
 
 var target_version = null;
