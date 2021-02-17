13ada24b08b624596c44578e0cda45d16004ad77
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-02-17 01:15:02 -0600

    add files

---
+++ redirect.js
@@ -0,0 +1,26 @@
+var base_url = "https://www.adriangb.com/scikeras/";
+
+var current = "https://www.adriangb.com/scikeras/test.html";
+var rest = current.replace(base_url, "");
+
+var target_version = null;
+
+$.getJSON(base_url + "versions.json", function (versions) {
+    if (versions.hasOwnProperty("stable")) {
+        target_version = versions.stable;
+        return;
+    }
+    if (versions.hasOwnProperty("latest")) {
+        target_version = versions.latest;
+        return;
+    }
+});
+
+function maybeRedirect() {
+    if (((target_version !== null) && !(rest.startsWith("refs")))) {
+        // redirect
+        window.location.replace(base_url.concat(target_version, "/", rest));
+    }
+}
+
+window.addEventListener('load', function () { setTimeout(maybeRedirect, 25); }, false);
