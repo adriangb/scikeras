9358d6e20e65a6105e58f4c2fa3254de68fcd07e
Scott <stsievert@users.noreply.github.com>
2021-02-28 12:40:33 -0600

    better test

--- tests/test_simple_usage.py
+++ tests/test_simple_usage.py
@@ -3,6 +3,7 @@ import pytest
 import tensorflow as tf
 
 from sklearn.datasets import make_classification
+from sklearn.preprocessing import OneHotEncoder
 
 from scikeras.wrappers import KerasClassifier
 
@@ -11,7 +12,6 @@ N_CLASSES = 4
 FEATURES = 8
 n_eg = 100
 X = np.random.uniform(size=(n_eg, FEATURES)).astype("float32")
-y = np.random.choice(N_CLASSES, size=n_eg).astype(int)
 
 
 def clf(single_output=False):
@@ -27,29 +27,36 @@ def clf(single_output=False):
     return model
 
 
-def test_classifier_only_model_specified():
+@pytest.mark.parametrize(
+    "use_case",
+    [
+        "binary_classification",
+        "binary_classification_w_one_class",
+        "classification_w_1d_targets",
+        "classification_w_onehot_targets",
+    ],
+)
+def test_classifier_only_model_specified(use_case):
     """
-    This tests uses cases where KerasClassifier works with the default loss.
-    It works for the following cases:
-
-    * binary classification
-    * one hot classification
-    * single class classification
-
+    Test uses cases where KerasClassifier works with the default loss.
     """
-    est = KerasClassifier(model=clf)
+
+    model__single_output = True if "binary" in use_case else False
+    if use_case == "binary_classification":
+        y = np.random.choice(2, size=len(X)).astype(int)
+    elif use_case == "binary_classification_w_one_class":
+        y = np.zeros(len(X))
+    elif use_case == "classification_w_1d_targets":
+        y = np.random.choice(N_CLASSES, size=len(X)).astype(int)
+    elif use_case == "classification_w_onehot_targets":
+        y = np.random.choice(N_CLASSES, size=len(X)).astype(int)
+        y = OneHotEncoder(sparse=False).fit_transform(y.reshape(-1, 1))
+    else:
+        raise ValueError("use_case={use_case} not recognized")
+
+    est = KerasClassifier(model=clf, model__single_output=model__single_output)
     est.partial_fit(X, y=y)
     assert est.current_epoch == 1
-
-    for y2 in [
-        np.random.choice(2, size=len(X)).astype(int),
-        (np.random.choice(2, size=len(X)).astype(int) * 2 - 1),
-        np.ones(len(X)).astype(int),
-        np.zeros(len(X)).astype(int),
-    ]:
-        est = KerasClassifier(model=clf, model__single_output=True)
-        est.partial_fit(X, y=y2)
-        assert est.current_epoch == 1
 
 
 def test_classifier_raises_for_single_output_with_multiple_classes():
@@ -58,6 +65,7 @@ def test_classifier_raises_for_single_output_with_multiple_classes():
     in the target (duh).
     """
     est = KerasClassifier(model=clf, model__single_output=True)
+    y = np.random.choice(N_CLASSES, size=len(X))
     msg = (
         "The model is configured to have one output, but the "
         "loss='categorical_crossentropy' is expecting multiple outputs "
